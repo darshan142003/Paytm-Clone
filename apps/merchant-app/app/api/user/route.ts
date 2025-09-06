46631f1c95f51141006691b988fa6210ee592592
Darsh <darshanpatil142003@gmail.com>
2025-09-06 12:10:47 +0530

    test8

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server"
 import prisma from "@repo/db/client"
 import { randomUUID } from "crypto"
 
+
 export const GET = async () => {
     await prisma.merchant.create({
         data: {
